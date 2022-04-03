2b96550801c52b45846e5da0c0c647146c2d9de6
Herlan Banegas <herlan.banegas21711@gmail.com>
2022-04-03 19:07:10 -0400

    First_Java_Program

---
+++ Week1/First_Java_Program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Herlan Banegas");
+        System.out.println("I am 29 years old");
+        System.out.println("My Hometown is Jersey City, Nj");
+    }
+}
